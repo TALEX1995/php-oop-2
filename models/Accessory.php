add4a89e94b057a75525b0d11c6fa3f126534c5a
TALEX1995 <tanase.alex@libero.it>
2023-07-24 17:01:43 +0200

    Added class Accessory

---
+++ models/Accessory.php
@@ -0,0 +1,26 @@
+<?php
+
+require_once __DIR__ . '/Product.php';
+
+class Accesory extends Product
+{
+    private $description;
+    private $material;
+
+    public function __construct(int $weight, int $price, string $image, $animal, string $description, string $material)
+    {
+        parent::__construct($weight, $price, $image, $animal);
+        $this->description = $description;
+        $this->material = $material;
+    }
+
+    public function getDescription()
+    {
+        return $this->description;
+    }
+
+    public function getMaterial()
+    {
+        return $this->material;
+    }
+}
