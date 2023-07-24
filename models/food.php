8b23cb766604345dae9f835b61774c48d9482e47
TALEX1995 <tanase.alex@libero.it>
2023-07-24 16:26:44 +0200

    Added method foos

--- models/food.php
+++ models/food.php
@@ -17,4 +17,49 @@ class Food extends Product
         $this->setIngredients($ingredients);
         $this->setAnimal($animal);
     }
+
+    private function setNetWeight($weight)
+    {
+        $success = parent::isNumberValid($weight);
+        if ($success) {
+            $this->net_weight = $weight;
+            return true;
+        }
+        return false;
+    }
+
+    public function getNetWeight()
+    {
+        return $this->net_weight;
+    }
+
+    private function setIngredients($ingredients)
+    {
+        if ((is_string($ingredients)) || (is_array($ingredients))) {
+            $this->ingredients = $ingredients;
+            return true;
+        }
+        return false;
+    }
+
+    public function getIngredients()
+    {
+        return $this->ingredients;
+    }
+
+
+    private function setAnimal($animal)
+    {
+        $animal_type = ['Cat', 'Dog', 'Bird', 'Fish'];
+        if (in_array($animal, $animal_type) || is_array($animal)) {
+            $this->$animal = $animal;
+            return true;
+        }
+        return false;
+    }
+
+    public function getAnimal()
+    {
+        return $this->animal;
+    }
 }
