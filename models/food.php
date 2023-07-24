683e990a3d0bfd4d46f572229a780a4183a3ad41
TALEX1995 <tanase.alex@libero.it>
2023-07-24 16:01:10 +0200

    Added construct food

--- models/food.php
+++ models/food.php
@@ -7,4 +7,14 @@ class Food extends Product
 {
     private $net_weight;
     private $ingredients;
+    private $animal;
+
+    // Construct
+    public function __construct(int $weight, int $price, string $image, int $net_weight, $ingredients, string $animal)
+    {
+        parent::__construct($weight, $price, $image);
+        $this->setNetWeight($net_weight);
+        $this->setIngredients($ingredients);
+        $this->setAnimal($animal);
+    }
 }
